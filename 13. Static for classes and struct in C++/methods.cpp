cc5cc3c7abf7a12214bb17c5b7460b2e0c9ee330
Bhumika-Kirola <kirolabhumika9@gmail.com>
2026-08-11 00:26:53 +0530

    Add notes and examples on static methods and non-static variable limitations

--- 13. Static for classes and struct in C++/methods.cpp
+++ 13. Static for classes and struct in C++/methods.cpp
@@ -1,14 +1,30 @@
+/*
+=============================================================
+TOPIC: Static Methods in C++ (Theory & Notes)
+
+1. Static Methods Overview:
+- When you make a method static, it works fine with static variables.
+- In the case of non-static variables, static methods cannot get access to them.
+- Why? Because static methods don't have a 'this' parameter, so they don't know which object instance they have to look into.
+- Effectively, it is like a function written outside a class.
+- To access non-static variables from a static method, you must explicitly pass an object 
+  instance into the method as a parameter.
+============================================================= */
+
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 
-class entity{
+struct entity{
     static int x, y;
     int a, b;
 
-    static void print(){
-        cout << x << y;
+    static void print(){    // print(entity& e)
+        cout << x << ", " << y;
+       //cout << a << ", "<< b;  //thsi will give out the error as the static method dont know which object instance they have look into as they dont have this parameter
     }
 };
 
@@ -18,5 +34,7 @@ int entity::y;
 int main(){
     entity e;
     entity e1;
-
+    entity::x=5;
+    entity::y=6;
+    entity::print(); // entity::print(e);
 }
--- 13. Static for classes and struct in C++/variable.cpp
+++ 13. Static for classes and struct in C++/variable.cpp
@@ -1,3 +1,22 @@
+/*
+=============================================================
+TOPIC: Static in Class/Struct (Theory & Notes)
+
+1. What is Static in a Class/Struct?
+- If you use static for variable in class/struct, there is only going to be one shared memory of that variable across all instances of the class (shared memory). that means whatever changes you make into the instance of object regarding that memory it will reflect across all the instances.
+- A static variable behaves like a global variable that belongs to the class.
+
+2. Behavior and Shared Memory:
+- Whatever changes you do to the object instance regarding that variable, those changes are going to reflect throughout the object instances in that variable case.
+- All the object instances of that variable will have shared memory.
+  
+3. External Definition Requirement (Linker Rule):
+- Declaring a static variable inside a class is not enough; you must define it outside 
+  the class (e.g., int entity::x;). 
+- Without this definition, you will get an "unresolved external symbol" Linker error.
+============================================================= */
+
+
 #include <iostream>
 
 using namespace std;
@@ -9,9 +28,10 @@ struct entity{
     }
 };
 
-int entity::x;
-int entity::y;
+int entity::x;    //External definition requirement: static variables need to be defined
+int entity::y;    //without proper definition can cause a compilation/linker error.
 int main(){
+
     entity e;
     e.x=3;
     e.y=4;
