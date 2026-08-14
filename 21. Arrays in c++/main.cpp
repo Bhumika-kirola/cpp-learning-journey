2197c1ad1992fb5764be19490f761aebbbf5d857
Bhumika-Kirola <kirolabhumika9@gmail.com>
2026-08-14 23:47:07 +0530

    Add incomplete array practice code

---
+++ 21. Arrays in c++/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int element[5];
+    for (int i = 0; i < 5; i++)
+    {
+        element[i] = 2;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << element[i] << endl;
+    }
+}
